34ff20402d444870781dc37b65f13a25b94a888e
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-11-18 16:24:47 -0300

    first requisites

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import About from './About';
+import Users from './Users';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/users/:ship" render={(props) => <Users {...props} message="Hello eveyone" />} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
+
 
 export default App;
--- src/Users.js
+++ src/Users.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const Users = () => (
-  <div>
-    <h2> Users </h2>
-    <p> My awesome Users component </p>
-  </div>
-);
-
-export default Users;
+export default class Users extends React.Component {
+  render() {
+    const { ship } = this.props.match.params
+    return (
+      <div>
+        <h2> Users </h2>
+        <p> {this.props.message}, this is my awesome {ship} </p>
+      </div>
+    )
+  }
+}
